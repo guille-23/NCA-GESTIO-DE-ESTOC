d3b73b52bf9298bd3253845505bae2985ff35989
ShadowTaiguer <131383826+ShadowTaiguer@users.noreply.github.com>
2023-12-05 09:19:13 +0100

    adsfn
    poner boton actualizar i eliminar

--- views/producte/mostrar.php
+++ views/producte/mostrar.php
@@ -35,13 +35,13 @@
             echo "<td>" . $row["Aula"] . "</td>";
             echo "<td>" . $row["Armari"] . "</td>";
             echo "<td>" . $row["Data_registre"] . "</td>";
+            echo '<td><a class="btn btn-warning" href="index.php?controller=producte&action=actualitzar&user_id=' . $row["id"] . '" role="button">Actualizar</a></td>';
+            echo '<td><a class="btn btn-danger" href="index.php?controller=producte&action=EsborrarUsuari&user_id=' . $row["id"] . '" role="button">Eliminar</a></td>';
             echo "</tr>";
         }
         ?>
     </table>
     <br><a class="btn btn-primary btn-lg active" href="index.php?controller=producte&action=insertar">Insertar</a></button>
-    <br><a class="btn btn-primary btn-lg active" href="index.php">Tornar</a></button>
-
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
 </body>
