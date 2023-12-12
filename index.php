b5a7062b5ba739460a720a84b7102307674333bb
ShadowTaiguer <131383826+ShadowTaiguer@users.noreply.github.com>
2023-12-12 12:08:42 +0100

    asd
    filtres

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
     <meta name="author" content="contain by Wepora team">
     <meta name="copyright" content="Copyright © 2020 Wepora" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.css" />
+    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.js"></script>
     <title>Wepora Snippets</title>
 
     <style>
@@ -36,6 +36,7 @@
         }
     </style>
 </head>
+
 <body>
     <!--content start-->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -72,61 +73,6 @@
     }
 
     ?>
-
-    <!--content end-->
-    <!-- JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
-
-    <div id="filtro-resultados">
-        <!-- Aquí se mostrarán los resultados del filtro -->
-    </div>
-
-    <!-- Agrega esto al final de tu contenido existente -->
-    <form id="filtro-form">
-        <label for="aula">Filtrar por Aula:</label>
-        <input type="text" id="aula" name="aula">
-
-        <label for="armari">Filtrar por Armari:</label>
-        <input type="text" id="armari" name="armari">
-
-        <button type="button" onclick="filtrarProductos()">Filtrar</button>
-    </form>
-
-    <script>
-        function filtrarProductos() {
-            var aula = document.getElementById('aula').value;
-            var armari = document.getElementById('armari').value;
-
-            $.ajax({
-                url: 'filtrar_productos.php',
-                type: 'GET',
-                data: { aula: aula, armari: armari },
-                dataType: 'json',
-                success: function (response) {
-                    mostrarResultados(response);
-                },
-                error: function (error) {
-                    console.log('Error en la solicitud AJAX:', error);
-                }
-            });
-        }
-
-        function mostrarResultados(resultados) {
-            var html = '<h3>Resultados del Filtro:</h3><ul>';
-
-            if (resultados.length > 0) {
-                for (var i = 0; i < resultados.length; i++) {
-                    html += '<li>' + resultados[i].Nom_del_producte + ' - Aula: ' + resultados[i].Aula + ' - Armari: ' + resultados[i].Armari + '</li>';
-                }
-            } else {
-                html += '<li>No se encontraron resultados.</li>';
-            }
-
-            html += '</ul>';
-
-            document.getElementById('filtro-resultados').innerHTML = html;
-        }
     </script>
 </body>
 
--- views/producte/mostrar.php
+++ views/producte/mostrar.php
@@ -14,11 +14,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.css" />
+    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.js"></script>
     <title>Wepora Snippets</title>
     </head>
 
 <body>
-<table class="table">
+<table class="table" id="myTable">
+    <thead>
     <tr>
         <th>Nom_del_producte</th>
         <th>Cuantitat</th>
@@ -26,8 +29,12 @@
         <th>Armari</th>
         <th>Data_registre</th>
         <th>Foto</th>
+        <th>Actualizar</th>
+        <th>Esborrar</th>
 
     </tr>
+    </thead>
+    <tbody>
     <?php
         while ($row = $productes->fetch_assoc()) {
 
@@ -44,7 +51,16 @@
             echo "</tr>";
         }
         ?>
+        </tbody>
     </table>
+    <script>
+        $(document).ready( function () {
+    $('#myTable').DataTable();
+} );
+    </script>
+    
+
+
     <br><a class="btn btn-primary btn-lg active" href="index.php?controller=producte&action=insertar">Insertar</a></button>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
